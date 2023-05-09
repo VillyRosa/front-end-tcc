42f027fce3054e893e3c850d59d13890df761564
Villy Rosa <82731258+VillyRosa@users.noreply.github.com>
2023-05-09 12:18:09 -0300

    Add files via upload

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -3,27 +3,31 @@ import PostPreview from '../../layout/main/PostPreview';
 
 import styles from './Home.module.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Home() {
 
     const [posts, setPosts] = useState([]);
 
-    (async function getPosts() {
-
-        await fetch(`http://localhost/tcc/back-end/routes/posts/posts.php`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
+    // Função para pegar os posts da api
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const response = await fetch(`http://localhost/tcc/back-end/routes/posts/posts.php`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                const data = await response.json();
+                setPosts(data.data);
+                console.log(data);
+            } catch (err) {
+                console.log(err);
             }
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setPosts(data.data);
-        })
-        .catch((err) => console.log(err))
-
-    })()
+        }
+        fetchPosts();
+    }, []);
 
     return (
         <main className={styles.main}>
@@ -36,6 +40,8 @@ function Home() {
                         <PostPreview 
                          key={post}
                          title={post.titulo_post}
+                         content={post.conteudo_post}
+                         datetime={post.datahora_post}
                         />
 
                 )))
