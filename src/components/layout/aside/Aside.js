d7f3235b77ee37986906bab78ba845fbb3fd9217
Villy Rosa <82731258+VillyRosa@users.noreply.github.com>
2023-05-16 11:03:59 -0300

    update aside

--- src/components/layout/aside/Aside.js
+++ src/components/layout/aside/Aside.js
@@ -1,9 +1,18 @@
 import styles from './Aside.module.css';
 
+import { IoSearch } from "react-icons/io5";
+import PerfilCard from '../main/PerfilCard';
+
 function Aside() {
     return (
         <aside className={styles.aside}>
-            <p>Aside</p>
+            <div className={styles.inputSearchBox}>
+                <input type="text" placeholder='Pesquisar . . .' />
+                <IoSearch className={styles.icon} />
+            </div>
+
+            <PerfilCard userObj={{name: 'Villy'}} />
+
         </aside>
     );
 }
